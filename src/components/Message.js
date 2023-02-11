228b249681a89a745aeadf5853c7193845c36f4d
Jo <test@gmail.com>
2023-02-11 14:03:28 +1300

    styling edits

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,20 +1,13 @@
-
-import { Col, Row} from "react-bootstrap";
-
-
+import { Col } from "react-bootstrap";
 const Message = () => {
   
   return (
-      <Col lg={12}>
+    <Col xl={12} sm={12} md={12}>
         <div className="message-bx wow slideInUp">
-          <Row>
-            <Col lg={12} md={12} xl={12}>
               <h3>Thank you for visiting my webpage.<br></br>I am looking forward in hearing from you.</h3>
-            </Col>
-            
-          </Row>
         </div>
-      </Col>
+    </Col>
+      
   )
 }
 
